b8a245996f2f78f4bc6f1dc5dfabb0b7b9341e45
Graham Anderson <grahama@me.com>
2016-06-08 09:58:10 -0400

    adding comment originally written in jupyter

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,3 +1,5 @@
+# Code orgininally writen in ../jupyter
+
 # The football.csv file contains the results from the English Premier League. 
 # The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
